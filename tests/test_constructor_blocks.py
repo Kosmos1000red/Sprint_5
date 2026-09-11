c3ecc231860e695a29185d9006feec6117ec5442
Marina Bobkova <marywolf97@mail.ru>
2026-09-11 11:23:22 +0300

    Хелперы вынечены во внещний файл helpers.py, в файлах test_login и test_constructor)blocks внесены изменения

--- tests/test_constructor_blocks.py
+++ tests/test_constructor_blocks.py
@@ -1,5 +1,6 @@
 import pytest
 from locators import Locators as L
+from helpers import click_tab_and_wait_active
 
 
 @pytest.mark.constructor
@@ -7,33 +8,26 @@ class TestConstructor:
 
     def test_tab_buns(self, open_constructor):
         driver = open_constructor
-        tab = driver.find_element(*L.CONSTRUCTOR_BUNS_BUTTON)
-        driver.execute_script(
-            "arguments[0].scrollIntoView({block: 'center'});", tab
-        )
-        tab.click()
+        # Сначала уходим на «Соусы», чтобы смена вкладки была заметной
+        click_tab_and_wait_active(driver, L.CONSTRUCTOR_SAUCES_BUTTON)
+        click_tab_and_wait_active(driver, L.CONSTRUCTOR_BUNS_BUTTON)
 
-        assert "tab_type_current" in tab.get_attribute("class"), \
-            "Вкладка 'Булки' не стала активной."
+        assert "tab_type_current" in driver.find_element(
+            *L.CONSTRUCTOR_BUNS_BUTTON
+        ).get_attribute("class"), "Вкладка 'Булки' не стала активной."
 
     def test_tab_sauces(self, open_constructor):
         driver = open_constructor
-        tab = driver.find_element(*L.CONSTRUCTOR_SAUCES_BUTTON)
-        driver.execute_script(
-            "arguments[0].scrollIntoView({block: 'center'});", tab
-        )
-        tab.click()
+        click_tab_and_wait_active(driver, L.CONSTRUCTOR_SAUCES_BUTTON)
 
-        assert "tab_type_current" in tab.get_attribute("class"), \
-            "Вкладка 'Соусы' не стала активной."
+        assert "tab_type_current" in driver.find_element(
+            *L.CONSTRUCTOR_SAUCES_BUTTON
+        ).get_attribute("class"), "Вкладка 'Соусы' не стала активной."
 
     def test_tab_fillings(self, open_constructor):
         driver = open_constructor
-        tab = driver.find_element(*L.CONSTRUCTOR_INGR_BUTTON)
-        driver.execute_script(
-            "arguments[0].scrollIntoView({block: 'center'});", tab
-        )
-        tab.click()
+        click_tab_and_wait_active(driver, L.CONSTRUCTOR_INGR_BUTTON)
 
-        assert "tab_type_current" in tab.get_attribute("class"), \
-            "Вкладка 'Начинки' не стала активной."
+        assert "tab_type_current" in driver.find_element(
+            *L.CONSTRUCTOR_INGR_BUTTON
+        ).get_attribute("class"), "Вкладка 'Начинки' не стала активной."
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,50 +1,40 @@
 import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from data import Data
 from locators import Locators as L
+from helpers import login, wait_logged_in
 
 
 @pytest.mark.login
 class TestLogin:
 
-    def _login(self, driver):
-        driver.find_element(*L.LOGIN_EMAIL_INPUT).send_keys(Data.EMAIL)
-        driver.find_element(*L.LOGIN_PASSWORD_INPUT).send_keys(Data.PASSWORD)
-        driver.find_element(*L.LOGIN_LOGIN_BUTTON).click()
-
     def test_login_via_main_button(self, driver):
         driver.get(Data.APP_URL)
         driver.find_element(*L.MAIN_ACCOUNT_BUTTON).click()
-        self._login(driver)
+        login(driver)
 
-        assert WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(L.MAIN_ORDER_BUTTON)
-        ), "Вход через кнопку 'Войти в аккаунт' не удался."
+        assert wait_logged_in(driver), \
+            "Вход через кнопку 'Войти в аккаунт' не удался."
 
     def test_login_via_account_button(self, driver):
         driver.get(Data.APP_URL)
         driver.find_element(*L.MAIN_PROFILE_BUTTON).click()
-        self._login(driver)
+        login(driver)
 
-        assert WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(L.MAIN_ORDER_BUTTON)
-        ), "Вход через кнопку 'Личный кабинет' не удался."
+        assert wait_logged_in(driver), \
+            "Вход через кнопку 'Личный кабинет' не удался."
 
     def test_login_via_registration_link(self, driver):
         driver.get(Data.REG_URL)
         driver.find_element(*L.REG_LOGIN_LINK).click()
-        self._login(driver)
+        login(driver)
 
-        assert WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(L.MAIN_ORDER_BUTTON)
-        ), "Вход через ссылку на форме регистрации не удался."
+        assert wait_logged_in(driver), \
+            "Вход через форму регистрации не удался."
 
     def test_login_via_forgot_pass_form(self, driver):
         driver.get(Data.PWD_RECOVERY_URL)
         driver.find_element(*L.RECOVERY_PAGE_LOGIN_LINK).click()
-        self._login(driver)
+        login(driver)
 
-        assert WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(L.MAIN_ORDER_BUTTON)
-        ), "Вход через форму восстановления пароля не удался."
+        assert wait_logged_in(driver), \
+            "Вход через форму восстановления пароля не удался."
